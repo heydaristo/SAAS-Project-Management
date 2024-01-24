00c79c36fa49d10d1cbd4ec14146aa6bdb28da1a
heydaristo <heydaristo@gmail.com>
2024-01-24 21:31:41 +0700

    fix merge

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -25,31 +25,24 @@
               @csrf
               <div class="mb-3">
                 <label class="form-label">Email address</label>
-<<<<<<< HEAD
                 <input type="email" class="form-control  @error('email')is-invalid @enderror"  name="email" placeholder="your@email.com" autocomplete="off" required value="{{ old('email')}}">
                 @error('email')
                     <div class="invalid-feedback">
                       {{ $message }}
                   </div>
                   @enderror
-=======
                 <input type="email" class="form-control" name="email" placeholder="your@email.com" autocomplete="off">
->>>>>>> 3d8a7ede17c524d976e9850008eb30a29607b9e3
               </div>
               <div class="mb-2">
                 <label class="form-label">
                   Password
                 <div class="input-group input-group-flat">
-<<<<<<< HEAD
                   <input type="password" class="form-control"  placeholder="Your password"  id="password" name="password" autocomplete="off" required>
                   @error('password')
                   <div class="invalid-feedback">
                     {{ $message }}
                 </div>
                 @enderror
-=======
-                  <input type="password" class="form-control"  placeholder="Your password" name="password"  autocomplete="off">
->>>>>>> 3d8a7ede17c524d976e9850008eb30a29607b9e3
                   <span class="input-group-text">
                     <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip" onclick="togglePassword('password')"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
