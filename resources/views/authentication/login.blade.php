423ed1cecfe58368cd869bc37906b2a991bbd35a
heydaristo <heydaristo@gmail.com>
2024-01-26 13:29:57 +0700

    fix dashboard

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -14,9 +14,9 @@
               </div>
               @endif
 
-            @if(session()->has('loginError'))
+            @if(session()->has('failed'))
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
-               {{ session('loginError') }}
+               {{ session('failed') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
               @endif
--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -13,7 +13,8 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Sign up - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <title>SAAS - FreelanceProjectManagement</title>
+    {{-- <title>Sign up - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title> --}}
     <script defer data-api="/stats/api/event" data-domain="preview.tabler.io" src="/stats/js/script.js"></script>
     <meta name="msapplication-TileColor" content="#0054a6"/>
     <meta name="theme-color" content="#0054a6"/>
