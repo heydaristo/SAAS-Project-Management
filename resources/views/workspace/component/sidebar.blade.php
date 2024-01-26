cde126f403ba86348a8bea20e0d9f29f67702b6b
heydaristo <heydaristo@gmail.com>
2024-01-26 16:09:10 +0700

    logout fix

--- resources/views/workspace/component/sidebar.blade.php
+++ resources/views/workspace/component/sidebar.blade.php
@@ -120,7 +120,7 @@
           <a href="#" class="dropdown-item">Feedback</a>
           <div class="dropdown-divider"></div>
           <a href="./settings.html" class="dropdown-item">Settings</a>
-          <a href="./sign-in.html" class="dropdown-item">Logout</a>
+          <a href="{{route('workspace.logout')}}" class="dropdown-item">Logout</a>
         </div>
       </div>
     </div>
