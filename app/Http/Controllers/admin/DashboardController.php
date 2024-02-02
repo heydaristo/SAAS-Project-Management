ba4b10a579354bec2a4cf3d65fe0f9fd119da54a
attaf-riski <atafriski27@gmail.com>
2024-02-02 16:53:45 +0700

    + add dynamic chart(not finished yet)

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -5,11 +5,32 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Charts\RevenueChart;
+use App\Models\TransactionAdmin;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(RevenueChart $revenueChart)
     {
-        return view('admin.dashboard', ['revenueChart' => $revenueChart->build()]);
+        // revenue summary
+        $revenueSummaryThreeMonths = TransactionAdmin::where('status', 'PAID')
+            ->where('date', '>=', Carbon::now()->subMonth(3))
+            ->sum('amount');
+
+        $revenueSummaryThirtyDays = TransactionAdmin::where('status', 'PAID')
+            ->where('date', '>=', Carbon::now()->subDays(30))
+            ->sum('amount');
+
+        $revenueSummarySevenDays = TransactionAdmin::where('status', 'PAID')
+            ->where('date', '>=', Carbon::now()->subDays(7))
+            ->sum('amount');
+
+        return view('admin.dashboard', [
+            'revenueSummaryThreeMonths' => $revenueSummaryThreeMonths,
+            'revenueSummaryThirtyDays' => $revenueSummaryThirtyDays,
+            'revenueSummarySevenDays' => $revenueSummarySevenDays,
+            'revenueChartThreeMonths' => $revenueChart->ThreeMonths(),
+            'revenueChartThirtyDays' => $revenueChart->ThirtyDays(),
+            'revenueChartSevenDays' => $revenueChart->SevenDays(),]);
     }
 }
