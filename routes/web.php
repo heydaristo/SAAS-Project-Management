02c5862accec792c16363afdba84a1a3cdef21a7
attaf-riski <atafriski27@gmail.com>
2024-02-21 11:10:46 +0700

    + not really important commit

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\QuotationController;
+use App\Http\Controllers\ContractController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\SubscriptionController;
@@ -33,13 +34,13 @@ Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/', [UserController::class,'login'])->name('/');
+    Route::get('/', [UserController::class, 'login'])->name('/');
     Route::get('/login', [UserController::class, 'login'])->name('login');
     Route::post('/login-proses', [UserController::class, 'login_proses'])->name('login-proses');
     Route::get('/forgot-password', [UserController::class, 'forgotPasswordShow'])->name('forgot-password');
     Route::post('/forgot-password', [UserController::class, 'forgotPassword'])->name('forgot-password.store');
     Route::get('/reset-password/{token}', [UserController::class, 'resetPassword'])->name('password.reset');
-    Route::post('/reset-password', [UserController::class,'resetPasswordProses'])->name('password.reset.store');
+    Route::post('/reset-password', [UserController::class, 'resetPasswordProses'])->name('password.reset.store');
 });
 
 Route::middleware(['guest'])->name('password.reset')->prefix('reset-password')->group(function () {
@@ -51,7 +52,7 @@ Route::get('/register', [UserController::class, 'register'])->name('register')->
 Route::post('/register-proses', [UserController::class, 'register_proses'])->name('register-proses')->middleware('guest');
 
 
-Route::group(['prefix' => 'workspace', 'middleware' => ['auth'], 'as' => 'workspace.'], function(){
+Route::group(['prefix' => 'workspace', 'middleware' => ['auth'], 'as' => 'workspace.'], function () {
 
     Route::get('/dashboard', [WorkspaceDashboardController::class, 'index'])->name('dashboard');
 
@@ -83,6 +84,8 @@ Route::group(['prefix' => 'workspace', 'middleware' => ['auth'], 'as' => 'worksp
 
     Route::post('/projects/store', [ProjectController::class, 'store'])->name('projects.store');
 
+    // quotations
+
     Route::get('/quotation', [QuotationController::class, 'index'])->name('quotation');
 
     Route::get('/quotation/showadd', [QuotationController::class, 'showadd'])->name('quotation.showadd');
@@ -106,8 +109,35 @@ Route::group(['prefix' => 'workspace', 'middleware' => ['auth'], 'as' => 'worksp
     Route::post('/quotation/editemail/{id}', [QuotationController::class, 'showEditEmail'])->name('quotation.editemail');
     Route::post('/quotation/sendemail', [QuotationController::class, 'sendEmail'])->name('quotation.sendemail');
 
-    Route::get('/invoice', [InvoiceController::class, 'index'])->name('invoice');
+    // contract
+
+    Route::get('/contract', [ContractController::class, 'index'])->name('contract');
+
+    // Route::get('/contract/showadd', [ContractController::class, 'showadd'])->name('quotation.showadd');
+
+    // Route::get('/contract/create', [ContractController::class, 'create'])->name('quotation.create');
+
+    // Route::get('/contract/edit/{id}', [ContractController::class, 'edit'])->name('quotation.edit');
+
+    // Route::get('/contract/update/{id}', [ContractController::class, 'update'])->name('quotation.update');
+
+    // Route::get('/contract/delete/{id}', [ContractController::class, 'delete'])->name('quotation.delete');
+
+    // Route::get('/contract/show/{id}', [ContractController::class, 'show'])->name('quotation.show');
+
+    // Route::post('/contract/store', [ContractController::class, 'store'])->name('quotation.store');
+    // Route::get('/contract/review/{id}', [ContractController::class, 'review'])->name('quotation.review');
+
+    // Route::get('/contract/pdf/{id}', [ContractController::class, 'pdf'])->name('quotation.pdf');
+
+    // Route::post('/contract/status/{id}', [ContractController::class, 'status'])->name('quotation.pdf');
+    // Route::post('/contract/editemail/{id}', [ContractController::class, 'showEditEmail'])->name('quotation.editemail');
+    // Route::post('/contract/sendemail', [ContractController::class, 'sendEmail'])->name('quotation.sendemail');
+
+    // invoice
     
+    Route::get('/invoice', [InvoiceController::class, 'index'])->name('invoice');
+
     Route::get('/invoice/show/{id}', [InvoiceController::class, 'showId'])->name('invoices.show');
 
     Route::post('/invoice/create', [InvoiceController::class, 'store'])->name('invoices.store');
@@ -144,10 +174,10 @@ Route::group(['prefix' => 'workspace', 'middleware' => ['auth'], 'as' => 'worksp
     Route::get('/subscriptions/success/{transactionid}', [SubscriptionController::class, 'success'])->name('subscriptions.success');
 
     // email
-    
+
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => ['admin'], 'as' => 'admin.'], function(){
+Route::group(['prefix' => 'admin', 'middleware' => ['admin'], 'as' => 'admin.'], function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/admindashboard', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -156,7 +186,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin'], 'as' => 'admin.'],
     Route::post('/users/create', [UserController::class, 'store'])->name('user.create');
     Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('user.delete');
     Route::put('/users/update/{id}', [UserController::class, 'update'])->name('user.update');
-    
+
     // plan management
     Route::get('/plans', [PlanController::class, 'index'])->name('plan.show');
     Route::post('/plans/create', [PlanController::class, 'store'])->name('plan.create');
@@ -176,59 +206,61 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin'], 'as' => 'admin.'],
     Route::put('/transactions/update/{id}', [AdminTranscationController::class, 'update'])->name('transaction.update');
 
     Route::get('/transactions/listsubscriptions/{id}', [AdminTranscationController::class, 'listSubscriptions'])->name('transaction.listsubscriptions');
-    
+
 });
 
-Route::group(['prefix' => 'superadmin', 'middleware' => ['superadmin'], 'as' => 'superadmin.'], function(){
-    Route::get('/', function () {
-        // Hitung jumlah pengguna yang mendaftar dalam satu minggu terakhir
-        $userCountLastWeek = User::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
-        
-        // Hitung jumlah total pengguna
-        $userCount = User::count();
-        
-        // Hitung jumlah klien yang didaftarkan dalam satu minggu terakhir
-        $clientCountLastWeek = Client::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
-        
-        // Hitung jumlah total klien
-        $clientCount = Client::count();
-        
-        // Mengirimkan data ke tampilan Blade
-        return view('superadmin.dashboard', [
-            'userCountLastWeek' => $userCountLastWeek,
-            'userCount' => $userCount,
-            'clientCountLastWeek' => $clientCountLastWeek,
-            'clientCount' => $clientCount
-        ]);
-     
-    });
+Route::group(
+    ['prefix' => 'superadmin', 'middleware' => ['superadmin'], 'as' => 'superadmin.'],
+    function () {
+        Route::get('/', function () {
+            // Hitung jumlah pengguna yang mendaftar dalam satu minggu terakhir
+            $userCountLastWeek = User::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
 
-    Route::get('/dashboard', function () {
-        // Hitung jumlah pengguna yang mendaftar dalam satu minggu terakhir
-        $userCountLastWeek = User::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
-        
-        // Hitung jumlah total pengguna
-        $userCount = User::count();
-        
-        // Hitung jumlah klien yang didaftarkan dalam satu minggu terakhir
-        $clientCountLastWeek = Client::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
-        
-        // Hitung jumlah total klien
-        $clientCount = Client::count();
-        
-        // Mengirimkan data ke tampilan Blade
-        return view('superadmin.dashboard', [
-            'userCountLastWeek' => $userCountLastWeek,
-            'userCount' => $userCount,
-            'clientCountLastWeek' => $clientCountLastWeek,
-            'clientCount' => $clientCount
-        ]);
-    })->name('dashboard');
+            // Hitung jumlah total pengguna
+            $userCount = User::count();
 
-    // admin management
-    Route::get('/admins', [AdminController::class, 'index'])->name('admin.show');
-    Route::post('/admins/create', [AdminController::class, 'store'])->name('admin.create');
-    Route::delete('/admins/delete/{id}', [AdminController::class, 'destroy'])->name('admin.delete');
-    Route::put('/admins/update/{id}', [AdminController::class, 'update'])->name('admin.update');
-}
+            // Hitung jumlah klien yang didaftarkan dalam satu minggu terakhir
+            $clientCountLastWeek = Client::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
+
+            // Hitung jumlah total klien
+            $clientCount = Client::count();
+
+            // Mengirimkan data ke tampilan Blade
+            return view('superadmin.dashboard', [
+                'userCountLastWeek' => $userCountLastWeek,
+                'userCount' => $userCount,
+                'clientCountLastWeek' => $clientCountLastWeek,
+                'clientCount' => $clientCount
+            ]);
+
+        });
+
+        Route::get('/dashboard', function () {
+            // Hitung jumlah pengguna yang mendaftar dalam satu minggu terakhir
+            $userCountLastWeek = User::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
+
+            // Hitung jumlah total pengguna
+            $userCount = User::count();
+
+            // Hitung jumlah klien yang didaftarkan dalam satu minggu terakhir
+            $clientCountLastWeek = Client::whereBetween('created_at', [Carbon::now()->subWeek(), Carbon::now()])->count();
+
+            // Hitung jumlah total klien
+            $clientCount = Client::count();
+
+            // Mengirimkan data ke tampilan Blade
+            return view('superadmin.dashboard', [
+                'userCountLastWeek' => $userCountLastWeek,
+                'userCount' => $userCount,
+                'clientCountLastWeek' => $clientCountLastWeek,
+                'clientCount' => $clientCount
+            ]);
+        })->name('dashboard');
+
+        // admin management
+        Route::get('/admins', [AdminController::class, 'index'])->name('admin.show');
+        Route::post('/admins/create', [AdminController::class, 'store'])->name('admin.create');
+        Route::delete('/admins/delete/{id}', [AdminController::class, 'destroy'])->name('admin.delete');
+        Route::put('/admins/update/{id}', [AdminController::class, 'update'])->name('admin.update');
+    }
 );
